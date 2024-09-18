6557230e0b897a22c48923867f970982a22a6f2f
KhatriSuson <susonkhatri110@gmail.com>
2024-09-18 14:09:54 +0545

    initial commit

--- mysql.session.sql
+++ mysql.session.sql
@@ -3,5 +3,12 @@
 
 -- DESCRIBE employes;
 
-USE office;
+-- USE office;
 
+-- SELECT * FROM departments;
+
+-- SELECT ProjectName, SUM(EstimatedCost) FROM office.projects WHERE ProjectName = 'Ecom' ORDER By EstimatedCost desc;
+
+-- SELECT DepartmentName, HeadOfDepartment, Budget, Location FROM office.departments WHERE HeadOfDepartment = 'sebika' ORDER By Budget desc;
+
+SELECT * FROM office.departments WHERE HeadOfDepartment = 'Nrabahadur' ORDER By Budget;
